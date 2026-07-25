9e652906865546fdece93395611e211e7e1104c8
Audrey Dutcher <audrey@rhelmot.io>
2026-07-25 14:13:57 -0700

    stub out json stuff for now

--- common.h
+++ common.h
@@ -3,13 +3,16 @@
 #include "cluster.h"
 #include "dfg.h"
 #include "intset.h"
-#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 
 bool parse_integer(const std::string &str, int &v, int a, int b);
 std::vector<std::string> split(const std::string &s, char c);
 bool fp_eq(double x, double y, double eps);
+
+#ifndef NO_JSON
+#include <nlohmann/json.hpp>
 void to_json(nlohmann::json &j, const SCluster &cluster);
 void to_json(nlohmann::json &j, const IOSubgraph &config);
 void to_json(nlohmann::json &j, const intset &s);
+#endif
